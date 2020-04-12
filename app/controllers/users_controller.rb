78be2bab146829154800fd0f162f45cd15991e79
github username <github email address>
2020-04-12 07:59:20 -0700

    User login and logout functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,23 @@ class UsersController < ApplicationController
         erb :'users/new.html'
        end
    end
+
+   get '/login' do 
+    erb :'users/login.html'
+   end
+
+   post '/login' do
+    @user = User.find_by(:email => params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/'
+        else
+            erb :'users/login.html'
+        end
+   end
+
+   get '/logout' do
+       session.clear
+       redirect '/'
+   end
 end
